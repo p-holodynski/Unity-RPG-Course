d15c5ec7c6b4cacf5d444029fa76f10c6da1d6e4
phjoker <phjok3r@gmail.com>
2018-06-16 17:00:48 +0100

    Fixed Camera
    Added Fixed Camera prefab

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void LateUpdate()
+	{
+        // lock camera to the player
+        transform.position = player.transform.position;
 	}
 }
